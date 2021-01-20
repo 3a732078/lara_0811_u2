2c4760d87b8688fead1b26338772abaadbd3c45f
Ben Peng <3a732078@gm.student.ncut.edu.tw>
2021-01-20 18:07:33 +0800

    練習1.3 串聯兩個路由

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    return redirect('r1');
+});
+
+Route::get('r1',function (){
+    return redirect('r2');
+});
+
+Route::get('r2',function (){
     return view('welcome');
 });
